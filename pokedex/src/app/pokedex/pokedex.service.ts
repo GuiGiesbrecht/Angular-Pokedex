f03fe5b4ccf9cc94015b89b514ad5821ba842f81
Guilherme Duarte Giesbrecht <guidgiesbrecht@gmail.com>
2023-01-30 22:38:18 -0300

    Change Service Function

--- pokedex/src/app/pokedex/pokedex.service.ts
+++ pokedex/src/app/pokedex/pokedex.service.ts
@@ -13,13 +13,20 @@ export class PokeApiService {
       this.httpClient.get<any>('https://pokeapi.co/api/v2/pokemon?limit=151')
     );
 
-    return req.results.map((pokemon: any) => {
-      const id = pokemon.url.split('/')[6];
-      return {
-        id,
-        name: pokemon.name,
-        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
-      };
-    });
+    const pokemonBasicList = await Promise.all(
+      req.results.map(async (pokemon: any) => {
+        const id = pokemon.url.split('/')[6];
+        console.log(id);
+        return {
+          id,
+          name: pokemon.name,
+          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+        };
+      })
+    );
+
+    console.log(pokemonBasicList);
+
+    return pokemonBasicList;
   }
 }
